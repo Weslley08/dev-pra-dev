6a2f88b4ca03de0a726200a84142517f7fa275f1
Weslley <weslleyjonathas88@gmail.com>
2021-11-19 18:23:51 -0300

    ....

--- src/main/java/br/com/devpradev/DevPraDevApplication.java
+++ src/main/java/br/com/devpradev/DevPraDevApplication.java
@@ -2,22 +2,12 @@ package br.com.devpradev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class DevPraDevApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevPraDevApplication.class, args);
-	}
-
-	@Bean
-	public PasswordEncoder getPasswordEncoder() {
-		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-		return encoder;
 	}
 
 }
