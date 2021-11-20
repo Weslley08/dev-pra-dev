e14589964664ac78ce10603cf75a9181749d127c
Weslley <weslleyjonathas88@gmail.com>
2021-11-20 18:03:34 -0300

    ...

--- src/main/java/br/com/devpradev/models/entity/Post.java
+++ src/main/java/br/com/devpradev/models/entity/Post.java
@@ -5,7 +5,6 @@ import java.util.Date;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotEmpty;
 
 import lombok.Data;
 
@@ -14,21 +13,20 @@ import lombok.Data;
 @Table(name = "posts")
 public class Post {
 
+
+  
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name = "id_post", unique=true)
   private Long idPost;
 
-  // * Definir tamanho maximo que o atributo pode receber(50 caracteres)
-  @NotEmpty
+  @Column(nullable = false)
   private String titulo;
-
-  // * Definir tamanho maximo que o atributo pode receber (50 caracteres)
-  @NotEmpty
+  
+  @Column(nullable = false)
   private String autor;
-
-  // * Definir tamanho maximo que o atributo pode receber(200 caracteres)
-  @NotEmpty
+  
+  @Column(nullable = false)
   private String texto;
 
   @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy - HH:mm", timezone = "America/Sao_Paulo")
