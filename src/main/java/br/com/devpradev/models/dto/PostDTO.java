184416ee7b73cbfd8c7477a805db7114316010a3
Weslley <weslleyjonathas88@gmail.com>
2021-11-20 19:54:40 -0300

    ....

--- src/main/java/br/com/devpradev/models/dto/PostDTO.java
+++ src/main/java/br/com/devpradev/models/dto/PostDTO.java
@@ -1,20 +1,13 @@
 package br.com.devpradev.models.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Data
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
 public class PostDTO {
-
 
     private Long idPost;
 
@@ -30,7 +23,6 @@ public class PostDTO {
     @Size(min = 30, max = 256)
     private String texto;
 
-    @Builder.Default
     private Date data = new Date();
     
 }
