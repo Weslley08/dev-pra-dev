9c8f9352c51c47a4bb19e3e7f1e07f1ad380ae3a
Weslley <weslleyjonathas88@gmail.com>
2021-11-22 02:56:49 -0300

    ...

--- src/main/java/br/com/devpradev/service/PostService.java
+++ src/main/java/br/com/devpradev/service/PostService.java
@@ -11,7 +11,6 @@ import lombok.AllArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
